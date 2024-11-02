f07f0468a953ed5b0623725e108899149a01b639
vitakurakina <talina.krina@gmail.com>
2024-11-02 11:09:20 +0300

    3table.php

--- lab2/3table.php
+++ lab2/3table.php
@@ -5,47 +5,30 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>The multiplication table</title>
-  <style>
-    table {
-      border: 2px solid black;
-      border-collapse: collapse;
-    }
-
-    th,
-    td {
-      padding: 10px;
-      border: 1px solid black;
-    }
-
-    th {
-      background-color: yellow;
-    }
-  </style>
 </head>
 <body>
   <h1>The multiplication table</h1>
   <?php
   $rows = 10;
   $cols = 10;
+  $color = '#FFD700';
+
+  static $count = 0;
+  $count++;
 
   echo "<table border='1' cellpadding='5' cellspacing='0'>";
-  for ($i = 0; $i <= $rows; $i++) {
-    echo "<tr>";
-    for ($j = 0; $j <= $cols; $j++) {
-      if ($i == 0 && $j == 0) {
-        echo "<th></th>";
-      } elseif ($i == 0) {
-        echo "<th>$j</th>";
-      } elseif ($j == 0) {
-        echo "<th>$i</th>";
-      } else {
-        echo "<td>" . ($i * $j) . "</td>";
+  for ($r = 1; $r <= $rows; $r++) {
+      echo "<tr>";
+      for ($c = 1; $c <= $cols; $c++) {
+          if ($r == 1 || $c == 1) {
+              echo "<th style='background-color: $color'>" . $r * $c . "</th>";
+          } else {
+              echo "<td>" . $r * $c . "</td>";
+          }
       }
-    }
-    echo "</tr>";
+      echo "</tr>";
   }
   echo "</table>";
-?>
+  ?>
 </body>
 </html>
-
